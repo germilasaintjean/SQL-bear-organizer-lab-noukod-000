0c617c3f1e708be97b30c9ed1653ea317f6d595f
Saint-Jean Germila <germila@noukod.com>
2018-11-12 07:51:25 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,6 +1,6 @@
 def selects_all_female_bears_return_name_and_age
   #{}"Write your SQL query here"
-  "SELECT female, name, age FROM bears;""SELECT bears.name, bears.age FROM bears WHERE bears.gender='F';"
+"SELECT bears.name, bears.age FROM bears WHERE bears.gender='F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
